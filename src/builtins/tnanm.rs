ea19d6175a1d84afb2c1a01c96ea5e6f2703c32f
tala <tala.aunv@gmail.com>
2025-09-04 10:33:35 +0300

    0-shell at beggining

--- src/builtins/tnanm.rs
+++ src/builtins/tnanm.rs
@@ -1,6 +1,9 @@
-
-
 pub fn tnanm() {
     const GREEN_NAMES: &str = include_str!("../../green names.txt");
     print!("{}", GREEN_NAMES);
+}
+
+pub fn z_shell() {
+    const zshell: &str = include_str!("../../0-shell.txt");
+    print!("{}", zshell);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 //tala
-use std::io::{self, Write};
+use std::io::{ self, Write };
 use std::process;
+use builtins::clear;
+use builtins::tnanm;
 mod builtins;
 mod shell;
 
 fn main() {
-    clear_screen();
+    builtins::clear::clear();
+    builtins::tnanm::z_shell();
     let mut sh = shell::Shell::new();
     if let Err(err) = sh.run() {
         eprintln!("{}", err);
         process::exit(1);
     }
-}
-
-fn clear_screen() {
-    // ESC[2J = clear screen, ESC[H = move cursor to home
-    print!("\x1b[2J\x1b[H");
-    let _ = io::stdout().flush();
 }
