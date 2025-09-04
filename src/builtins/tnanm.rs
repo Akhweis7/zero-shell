6ed8bf19ab372b2e61f2a3b5c6ba6e8306314b5f
tala <tala.aunv@gmail.com>
2025-09-04 11:16:42 +0300

    colorized tnanm & welcoming

--- src/builtins/tnanm.rs
+++ src/builtins/tnanm.rs
@@ -1,9 +1,9 @@
 pub fn tnanm() {
     const GREEN_NAMES: &str = include_str!("../../green names.txt");
-    print!("{}", GREEN_NAMES);
+    print!("\x1b[38;5;40m{}\x1b[0m", GREEN_NAMES); // ansi color. \x1b[0m to reset the color
 }
 
 pub fn z_shell() {
     const zshell: &str = include_str!("../../0-shell.txt");
-    print!("{}", zshell);
+    print!("\x1b[38;5;40m{}\x1b[0m", zshell); // ansi color. \x1b[0m to reset the color
 }
