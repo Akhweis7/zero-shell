30da02fca60628f673747c8f0ac22dfab8f58a6d
tala <tala.aunv@gmail.com>
2025-09-14 20:12:38 +0300

    fixed some errs

--- src/builtins/tnanm.rs
+++ src/builtins/tnanm.rs
@@ -1,9 +1,9 @@
 pub fn tnanm() {
-    // const GREEN_NAMES: &str = include_str!("../../green names.txt");
-    // print!("\x1b[38;5;40m{}\x1b[0m", GREEN_NAMES); // ansi color. \x1b[0m to reset the color
+    const GREEN_NAMES: &str = include_str!("../../green names.txt");
+    print!("\x1b[38;5;40m{}\x1b[0m", GREEN_NAMES); // ansi color. \x1b[0m to reset the color
 }
 
 pub fn z_shell() {
-    // const zshell: &str = include_str!("../../0-shell.txt");
-    // print!("\x1b[38;5;40m{}\x1b[0m", zshell); // ansi color. \x1b[0m to reset the color
+    const zshell: &str = include_str!("../../0-shell.txt");
+    print!("\x1b[38;5;40m{}\x1b[0m", zshell); // ansi color. \x1b[0m to reset the color
 }
